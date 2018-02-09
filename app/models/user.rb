1cb3b917136586a7250f92656ff3143d35e0a19d
Richard <rza47@sfu.ca>
2018-02-09 13:35:45 -0800

    Fixed regex to function properly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-    validates :name, presence: true, length: {maximum: 100}, format: {with: /\A[a-zA-Z ]+\Z/}
+    validates :name, presence: true, length: {maximum: 100}, format: {with: /(?=.*[a-zA-Z ]).+/}
     validates :email, presence: true, length: {maximum: 100}, format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i}, uniqueness: {case_sensitive: false}
 
     has_secure_password
-    validates :password, presence: true, length: {minimum: 7, maximum: 50}, format: {with: /\A[A-Za-z]+[A-Za-z]+[0-9]+\Z/}
+    validates :password, presence: true, length: {minimum: 7, maximum: 50}, format: {with: /(?=.*[A-Z])+(?=.*[a-z])+(?=.*[0-9]).+/}
 end
