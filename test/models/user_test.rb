2bafad5199d080f6236a805185602e7d2b8246bd
Richard <rza47@sfu.ca>
2018-02-10 13:57:37 -0800

    Adding tests for the user model.
    This to test to make sure the validations for each field of the user model is working as intended.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,44 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "reject empty name" do
+    @user = User.new(name: " ", email: "testing@test.com", password: "Testing1", password_confirmation: "Testing1")
+    assert_not @user.valid?
+  end
+
+  test "reject name that is too long" do
+    x = "x" * 101
+    @user = User.new(name: x, email: "testing@test.com", password: "Testing1", password_confirmation: "Testing1")
+    assert_not @user.valid?
+  end
+
+
+  test "reject empty email" do
+    @user = User.new(name: "bob", email: " ", password: "Testing1", password_confirmation: "Testing1")
+    assert_not @user.valid?
+  end
+
+  test "reject email that is too long" do
+    z = ("z" * 91) + "@email.com" 
+    @user = User.new(name: "bob", email: z, password: "Testing1", password_confirmation: "Testing1")
+    assert_not @user.valid?
+  end
+
+  test "reject blank password" do
+    @user = User.new(name: "bob", email: "testing@test.com", password: " ", password_confirmation: " ")
+    assert_not @user.valid?
+  end
+
+  test "reject password that is too short" do
+    @user = User.new(name: "bob", email: "testing@test.com", password: "Test1", password_confirmation: "Test1")
+    assert_not @user.valid?
+  end
+
+  test "reject password that is too long" do
+    y = "T" + ("x" * 50) + "1"
+    @user = User.new(name: "bob", email: "testing@test.com", password: y, password_confirmation: y)
+    assert_not @user.valid?
+  end
+
 end
