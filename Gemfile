39b5dab9b63d8edd646e7e40105526f6f912523e
Richard <rza47@sfu.ca>
2018-02-12 00:39:29 -0800

    Removing Bcrypt gem which resulted in a few other changes
    Edited the db for the users model so that it has a password field in place of the password_digest field. Also made some other changes to make it so that rails tests functions properly, such as replacing name with username. Finally, I also added one line of code in the routes file so that the sign up page functions properly.

--- Gemfile
+++ Gemfile
@@ -26,7 +26,7 @@ gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
-gem 'bcrypt', git: 'https://github.com/codahale/bcrypt-ruby.git', :require => 'bcrypt'
+# gem 'bcrypt', git: 'https://github.com/codahale/bcrypt-ruby.git', :require => 'bcrypt'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,42 +2,48 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-  test "reject empty name" do
-    @user = User.new(name: " ", email: "testing@test.com", password: "Testing1", password_confirmation: "Testing1")
+  test "reject empty username" do
+    @user = User.new(username: " ", email: "testing@test.com", password: "Testing1", password_confirmation: "Testing1")
     assert_not @user.valid?
   end
 
-  test "reject name that is too long" do
-    x = "x" * 101
-    @user = User.new(name: x, email: "testing@test.com", password: "Testing1", password_confirmation: "Testing1")
+  test "reject username that is too short" do
+    x = "x" * 2
+    @user = User.new(username: x, email: "testing@test.com", password: "Testing1", password_confirmation: "Testing1")
+    assert_not @user.valid?
+  end
+
+  test "reject username that is too long" do
+    x = "x" * 31
+    @user = User.new(username: x, email: "testing@test.com", password: "Testing1", password_confirmation: "Testing1")
     assert_not @user.valid?
   end
 
 
   test "reject empty email" do
-    @user = User.new(name: "bob", email: " ", password: "Testing1", password_confirmation: "Testing1")
+    @user = User.new(username: "bob", email: " ", password: "Testing1", password_confirmation: "Testing1")
     assert_not @user.valid?
   end
 
   test "reject email that is too long" do
-    z = ("z" * 91) + "@email.com" 
-    @user = User.new(name: "bob", email: z, password: "Testing1", password_confirmation: "Testing1")
+    z = ("z" * 21) + "@email.com" 
+    @user = User.new(username: "bob", email: z, password: "Testing1", password_confirmation: "Testing1")
     assert_not @user.valid?
   end
 
   test "reject blank password" do
-    @user = User.new(name: "bob", email: "testing@test.com", password: " ", password_confirmation: " ")
+    @user = User.new(username: "bob", email: "testing@test.com", password: " ", password_confirmation: " ")
     assert_not @user.valid?
   end
 
   test "reject password that is too short" do
-    @user = User.new(name: "bob", email: "testing@test.com", password: "Test1", password_confirmation: "Test1")
+    @user = User.new(username: "bob", email: "testing@test.com", password: "Test", password_confirmation: "Test")
     assert_not @user.valid?
   end
 
   test "reject password that is too long" do
-    y = "T" + ("x" * 50) + "1"
-    @user = User.new(name: "bob", email: "testing@test.com", password: y, password_confirmation: y)
+    y = "x" * 31
+    @user = User.new(username: "bob", email: "testing@test.com", password: y, password_confirmation: y)
     assert_not @user.valid?
   end
 
