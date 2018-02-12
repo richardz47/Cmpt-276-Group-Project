9ebfe03038adb8c2f72e3781ba67754780cbcfcf
Richard <rza47@sfu.ca>
2018-02-12 12:26:31 -0800

    Adding new tests for the user controller
    Will add more tests later when we're allowed to add in new gems.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,4 +6,18 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "reject invalid inputs" do
+    assert_no_difference('User.count') do
+      post users_path, params: {user: {username: "", email: "", password: "", password_confirmation: ""}}
+    end
+  end
+
+  test "new user should be created and redirected" do
+    assert_difference('User.count') do
+      post users_path, params: {user: {username: "test", email: "test@test.com", password: "12345", password_confirmation: "12345"}}
+    end
+    assert_redirected_to '/users/980190963' # This is always the user id my fixtures generated during testing, so I'll just leave
+                                            # the id as that for now.
+  end
+
 end
