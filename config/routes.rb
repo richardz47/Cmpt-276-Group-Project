ad89953c50fea56fdbc4e5b3ca07b6fd0b07e494
karanr97 <35911513+karanr97@users.noreply.github.com>
2018-02-13 19:53:43 -0800

    Added log in and log out routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  
+  #login form
+	get '/login' => 'sessions#new'
+	#logging in
+	post '/login' => 'sessions#create'
+	#logging out
+	get '/logout' => 'sessions#destroy'
 
   resources :users
 
