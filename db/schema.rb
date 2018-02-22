5bafd9b001f727d2783024dbd5ebe6bd2f7d2491
mta86 <mta86@sfu.ca>
2018-02-22 14:52:01 -0800

    explore more updated

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20180219042600) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
