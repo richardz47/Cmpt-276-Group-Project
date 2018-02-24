b652122e4a1394e5baf68aa5139837829f56bdd5
Richard <rza47@sfu.ca>
2018-02-24 11:31:33 -0800

    Adding some redirection for the events controller.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,17 @@
 class EventsController < ApplicationController
   def new
+    if !check_logged_in
+      redirect_to root_path
+    
+    else
+      @event = Event.new
+    end
   end
 
   def display
+    if !check_logged_in
+      redirect_to root_path
+    end
   end
 
   def show
