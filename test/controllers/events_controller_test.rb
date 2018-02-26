4512958c21c7245920e7f8cca21c37bdd97ee95e
Richard <rza47@sfu.ca>
2018-02-26 15:43:52 -0800

    Adding new tests for the events controller
    Also had to modify user tests and fixtures to accommodate the changes

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -2,18 +2,64 @@ require 'test_helper'
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get events_new_url
+    @test = users(:test)
+    post login_path, params: {email: @test.email, password: '12345'}
+    get newevent_path
     assert_response :success
+  end
+
+  test "shouldn't get new" do
+    get newevent_path
+    assert_redirected_to root_path
   end
 
   test "should get display" do
-    get events_display_url
+    @test = users(:test)
+    post login_path, params: {email: @test.email, password: '12345'}
+    get displayevents_url
     assert_response :success
   end
 
-  test "should get show" do
-    get events_show_url
+  test "shouldn't get display" do
+    get displayevents_url
+    assert_redirected_to root_path
+  end
+
+  test "valid new event" do
+    @test = users(:test)
+    post login_path, params: {email: @test.email, password: '12345'}
+    post newevent_path, params: {event: {name: "test", start_time: DateTime.now, end_time: DateTime.now }}
+    assert_equal(2, Event.count)
+    assert_redirected_to event_url(Event.last)
+  end
+
+  test "invalid new event" do
+    @test = users(:test)
+    post login_path, params: {email: @test.email, password: '12345'}
+    post newevent_path, params: {event: {name: "", start_time: DateTime.now, end_time: DateTime.now }}
+    assert_equal(1, Event.count)
+  end
+
+  test "should show event" do
+    @test = users(:test)
+    post login_path, params: {email: @test.email, password: '12345'}
+    @testevent = events(:test)
+    get event_url(@testevent)
     assert_response :success
   end
+
+  test "shouldn't show event w/o logged in" do
+    @testevent = events(:test)
+    get event_url(@testevent)
+    assert_redirected_to root_path
+  end
+
+  test "shouldn't show event if wrong user" do
+    @test = users(:test2)
+    post login_path, params: {email: @test.email, password: '12345'}
+    @testevent = events(:test)
+    get event_url(@testevent)
+    assert_redirected_to displayevents_url
+  end  
 
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -8,12 +8,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "reject invalid inputs" do
     post signup_path, params: {user: {username: "", email: "", password: "", password_confirmation: ""}}
-    assert_equal(1, User.count)
+    assert_equal(2, User.count)
   end
 
   test "new user should be created,logged in, and redirected" do
     post signup_path, params: {user: {username: "test", email: "test@test.com", password: "12345", password_confirmation: "12345"}}
-    assert_equal(2, User.count)
+    assert_equal(3, User.count)
     assert check_logged_in
     assert_redirected_to ("/users/" + ((@current_user.id).to_s)) 
   end
