5cfc83af9a531ed344896ad04f944f59cbdf829e
mta86 <mta86@sfu.ca>
2018-03-14 02:53:05 -0700

    button animation added

--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20180303182544) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
